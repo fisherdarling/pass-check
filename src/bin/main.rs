0979c018fe5eaaf051c98bef15a648cc99bf6423
Fisher Darling <fdarlingco@gmail.com>
2021-05-12 22:21:38 -0600

    Subcommands and searching

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,120 @@
-fn main() -> anyhow::Result<()> {
-    let target_path = std::env::args().nth(1).unwrap();
-    let entry_point = std::env::args().nth(2).unwrap();
+use std::{borrow::Borrow, path::PathBuf};
 
-    let directory = std::fs::read_dir(target_path)?;
-    pass_check::run(directory, entry_point)
+use clap::{AppSettings, Clap};
+use colored::Colorize;
+use pass_check::PassCheck;
+use regex::Regex;
+
+#[derive(Clap)]
+#[clap(
+    version = "0.1",
+    author = "Fisher D. <fdarling@mines.ed>, Jake V. <jvossen@mines.edu>"
+)]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// Target folder that contains the LLVM bitcode (.bc files).
+    /// Generally under `target/<profile>/deps`
+    #[clap(short, long, parse(from_os_str))]
+    target_dir: PathBuf,
+    /// Output json (used for comparisions).
+    #[clap(short, long)]
+    json: bool,
+    /// Output to a file.
+    #[clap(short, long, parse(from_os_str))]
+    output: Option<PathBuf>,
+    /// Do not print `Adding Module`
+    #[clap(short, long)]
+    silent: bool,
+    #[clap(subcommand)]
+    command: Subcommand,
+}
+
+#[derive(Clap)]
+enum Subcommand {
+    /// Preform analyses
+    Analyze(Analyze),
+    /// Search for demangled functions, module names, etc.
+    Search(Search),
+    /// Compare two different analysis outputs
+    Compare(Compare),
+}
+
+#[derive(Clap)]
+enum Analyze {
+    /// Analyze a single function. Expects a full demangled path. See `search` to find functions.
+    Func { func_name: String },
+    /// Analyze every function in a module. Expects the full module name. See `search` to find modules.
+    Module { module_name: String },
+    /// Analyze every module and function in the target folder
+    Folder,
+}
+
+#[derive(Clap)]
+enum Search {
+    /// Demangle a given path
+    Demangle { path: String },
+    /// Search for demangled functions with the given regex
+    Func {
+        /// Function search regex
+        regex: Regex,
+    },
+    /// Search for modules with the given regex
+    Module {
+        /// Module search regex
+        regex: Regex,
+    },
+}
+
+#[derive(Clap)]
+enum Compare {
+    Modules {
+        original_json: PathBuf,
+        new_json: PathBuf,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+
+    let dir = std::fs::read_dir(&opts.target_dir)?;
+    let modules = pass_check::read_modules(dir, opts.silent)?;
+
+    let pass_check = PassCheck::new(modules);
+    let analysis = pass_check.analysis();
+
+    match opts.command {
+        Subcommand::Analyze(_) => {}
+        Subcommand::Search(s) => match s {
+            Search::Demangle { path } => {
+                let demangled = pass_check.demangle(&path);
+
+                println!("{}", demangled);
+            }
+            Search::Func { regex } => {
+                let results = pass_check.search_for_func(&regex, &analysis);
+
+                if results.is_empty() {
+                    println!("{}", "No functions found.".red().bold());
+                } else {
+                    for result in results {
+                        println!("{}", result);
+                    }
+                }
+            }
+            Search::Module { regex } => {
+                let results = pass_check.search_for_module(&regex, &analysis);
+
+                if results.is_empty() {
+                    println!("{}", "No modules found.".red().bold());
+                } else {
+                    for result in results {
+                        println!("{}", result);
+                    }
+                }
+            }
+        },
+        Subcommand::Compare(_) => {}
+    }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,58 +7,122 @@ use std::{collections::HashMap, fs::ReadDir, path::Path};
 use colored::Colorize;
 use llvm_ir::Module;
 use llvm_ir_analysis::CrossModuleAnalysis;
+use regex::Regex;
 use rustc_demangle::demangle;
 
-pub fn run(directory: ReadDir, entry_point: String) -> anyhow::Result<()> {
-    let modules = read_modules(directory)?;
-    let analysis = CrossModuleAnalysis::new(&modules);
-
-    let mut mangle_map: HashMap<String, &str> = HashMap::new();
-
-    for function in analysis.functions() {
-        let demangled = format!("{:#}", demangle(&function.name));
-        mangle_map.insert(demangled, &function.name);
-    }
-
-    let entry_point = if let Some(name) = mangle_map.get(&entry_point) {
-        println!("{}", "Demangled and found Function:".green().bold());
-        println!(
-            "    {} => {}",
-            entry_point.white().bold(),
-            name.white().bold()
-        );
-
-        name
-    } else {
-        println!(
-            "{}: {}",
-            "Could not map given function".yellow().bold(),
-            entry_point
-        );
-        entry_point.as_str()
-    };
-    println!();
-
-    let mut context = Context::new(analysis);
-    let stats = context.analyze_function_by_name(entry_point).unwrap();
-
-    println!("{:#?}", stats);
-
-    Ok(())
+pub struct PassCheck {
+    modules: Vec<Module>,
 }
 
-fn read_modules(directory: ReadDir) -> anyhow::Result<Vec<Module>> {
+impl PassCheck {
+    pub fn new(modules: Vec<Module>) -> Self {
+        Self { modules }
+    }
+
+    // pub fn new(path: &Path) -> anyhow::Result<Self> {
+
+    //     Ok(PassCheck { modules })
+    // }
+
+    pub fn modules(&self) -> &[Module] {
+        &self.modules
+    }
+
+    pub fn analysis<'s>(&'s self) -> CrossModuleAnalysis<'s> {
+        CrossModuleAnalysis::new(self.modules())
+    }
+
+    pub fn demangle(&self, path: &str) -> String {
+        format!("{:#}", demangle(path))
+    }
+
+    pub fn search_for_func(
+        &self,
+        regex: &Regex,
+        analysis: &CrossModuleAnalysis<'_>,
+    ) -> Vec<String> {
+        let mut matches = Vec::new();
+
+        for function in analysis.functions() {
+            let demangled = format!("{:#}", demangle(&function.name));
+            if regex.is_match(&demangled) {
+                matches.push(demangled);
+            }
+        }
+
+        matches
+    }
+
+    pub fn search_for_module(
+        &self,
+        regex: &Regex,
+        analysis: &CrossModuleAnalysis<'_>,
+    ) -> Vec<String> {
+        let mut matches = Vec::new();
+
+        for module in analysis.modules() {
+            if regex.is_match(&module.name) {
+                matches.push(module.name.to_string());
+            }
+        }
+
+        matches
+    }
+}
+
+// pub fn run(directory: ReadDir, entry_point: String) -> anyhow::Result<()> {
+//     let modules = read_modules(directory)?;
+//     let analysis = CrossModuleAnalysis::new(&modules);
+
+//     let mut mangle_map: HashMap<String, &str> = HashMap::new();
+
+//     for function in analysis.functions() {
+//         let demangled = format!("{:#}", demangle(&function.name));
+//         mangle_map.insert(demangled, &function.name);
+//     }
+
+//     let entry_point = if let Some(name) = mangle_map.get(&entry_point) {
+//         println!("{}", "Demangled and found Function:".green().bold());
+//         println!(
+//             "    {} => {}",
+//             entry_point.white().bold(),
+//             name.white().bold()
+//         );
+
+//         name
+//     } else {
+//         println!(
+//             "{}: {}",
+//             "Could not map given function".yellow().bold(),
+//             entry_point
+//         );
+//         entry_point.as_str()
+//     };
+//     println!();
+
+//     let mut context = Context::new(analysis);
+//     let stats = context.analyze_function_by_name(entry_point).unwrap();
+
+//     println!("{:#?}", stats);
+
+//     Ok(())
+// }
+
+pub fn read_modules(directory: ReadDir, silent: bool) -> anyhow::Result<Vec<Module>> {
     let mut modules = Vec::new();
 
     for entry in directory.flatten() {
         if entry.file_type()?.is_file() && is_bitcode(&entry.path()) {
             let path = entry.path();
-            println!(
-                "    {} {} {}",
-                "Adding Module".green().bold(),
-                pretty_crate(&path),
-                &path.display().to_string().dimmed(),
-            );
+
+            if !silent {
+                println!(
+                    "    {} {} {}",
+                    "Adding Module".green().bold(),
+                    pretty_crate(&path),
+                    &path.display().to_string().dimmed(),
+                );
+            }
 
             match Module::from_bc_path(&entry.path()) {
                 Ok(m) => modules.push(m),
@@ -66,7 +130,10 @@ fn read_modules(directory: ReadDir) -> anyhow::Result<Vec<Module>> {
             }
         }
     }
-    println!();
+
+    if !silent {
+        println!();
+    }
 
     Ok(modules)
 }
