c086cd948c081cedc7e294f4c7746c4441e1b272
Jake Vossen <jake@vossen.dev>
2021-05-13 22:38:18 -0600

    moved logic back into main func

--- examples/sum/sum.rs
+++ examples/sum/sum.rs
@@ -1,15 +1,11 @@
 fn main() {
 
-    // let x = "10"
+    let x = "10".parse().unwrap();
 
-    let x = sum("10".parse().unwrap());
-    println!("{}", x);
-}
-
-fn sum(n: usize) -> usize {
     let mut total = 0;
-    for x in 1..n {
-        total += x;
+    for i in 1..x {
+        total += i;
     }
-    total
+
+    println!("{}", x);
 }
